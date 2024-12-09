f7f0adacac39f78910a911dabadb8dcd81999eca
FatemehEbrahimiLovesAI <fatemeh.ebrahimi.russophile@gmail.com>
2024-12-09 14:54:51 +0330

    Create University.py

---
+++ University/University.py
@@ -0,0 +1,122 @@
+# User authentication system in the faculty
+
+class University():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Faculty():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Course_unit():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+    
+
+class Field_of_study():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Student():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+      
+class President():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Personnel():
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Professor(Personnel):
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Guard(Personnel):
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class DinningHallStaff(Personnel):
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Cook(DinningHallStaff):
+    def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
+
+class Server(DinningHallStaff):
+     def __init__():
+        pass
+    
+    def __str__():
+        pass
+    
+    def __repr__():
+        pass
