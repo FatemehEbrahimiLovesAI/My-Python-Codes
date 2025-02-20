a8f5727b3186f8556068678bb34099e27a21f5a0
FatemehEbrahimiLovesAI <fatemeh.ebrahimi.russophile@gmail.com>
2025-02-20 17:41:55 +0330

    Update University.py
    what is updated?
    
    1- add some methodes to "University" class.
    
    2-update "__str__" and "__repr__" methodes of all classes.

--- University/University.py
+++ University/University.py
@@ -14,9 +14,51 @@ class University():
         self.address = address
 
     def __str__(self):
-        pass
+        return f"{self.name} university : address -> {self.address}"
 
     def __repr__(self):
+        return f"{self.__class__.__name__}({self.name},{self.address})"
+
+    def add_faculty(self):
+        pass
+
+    def add_course(self):
+        pass
+
+    def add_student(self):
+        pass
+
+    def add_professor(self):
+        pass
+
+    def add_cook(self):
+        pass
+
+    def add_server(self):
+        pass
+
+    def add_field(self):
+        pass
+
+    def remove_faculty(self):
+        pass
+
+    def remove_course(self):
+        pass
+
+    def remove_student(self):
+        pass
+
+    def remove_professor(self):
+        pass
+
+    def remove_cook(self):
+        pass
+
+    def remove_server(self):
+        pass
+
+    def remove_field(self):
         pass
 
 class Faculty():
@@ -28,10 +70,11 @@ class Faculty():
         self.location = location
 
     def __str__(self):
-        pass
+        return f"name: {self.name}\nuniversity: {self.university}\nlocation: {self.location}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.university},{self.location})"
+    
 
 class Course_unit():
     def __init__(self, name: str, faculties: list, lecturers: list):
@@ -40,10 +83,10 @@ class Course_unit():
         self.lecturers = lecturers
 
     def __str__(self):
-        pass
+        return f"course name: {self.name}\ncourse faculties: {self.faculties}\ncourse lecturers: {self.lecturers}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.faculties},{self.lecturers})"
 
 class Field_of_study():
     def __init__(self, name: str, courses: list):
@@ -51,10 +94,10 @@ class Field_of_study():
         self.courses = courses
 
     def __str__(self):
-        pass
+        return f"Field name: {self.name}\nField courses: {self.courses}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.courses})"
 
 class Student():
     def __init__(self, name, student_id, date_of_birth, year_of_entry, city, dorm_situation=False, dorm_location="don't have"):
@@ -66,20 +109,22 @@ class Student():
         self.dorm_location = dorm_location
 
     def __str__(self):
-        pass
+        return f"student name: {self.name}\nstudent ID: {self.student_id}\ndate of birth: {self.date_of_birth}\nyear of entry: {self.year_of_entry}\ndorm situation: {self.dorm_situation}\ndorm location: {self.dorm_location}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.student_id},{self.date_of_birth},{self.year_of_entry},{self.dorm_situation},{self.dorm_location})"
 
 class President():
-    def __init__(self):
-        pass
+    def __init__(self,name,university,date_of_birth):
+        self.name = name
+        self.university = university
+        self.date_of_birth = date_of_birth
 
     def __str__(self):
-        pass
+        return f"President name: {self.name}\nuniversity: {self.university}\ndate of birth: {self.date_of_birth}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.university},{self.date_of_birth})"
 
 class Personnel():
     def __init__(self, name, role):
@@ -87,10 +132,10 @@ class Personnel():
         self.role = role
 
     def __str__(self):
-        pass
+        return f"Personnel name: {self.name}\nPersonnel role: {self.role}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.role})"
 
 class Professor(Personnel):
     def __init__(self, name: str, courses: list):
@@ -98,49 +143,49 @@ class Professor(Personnel):
         self.courses = courses
 
     def __str__(self):
-        pass
+        return f"Professor name: {self.name}\nProfessor courses: {self.courses}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.courses})"
 
 class Guard(Personnel):
-    def __init__(self, name: str, guard_ID: str):
+    def __init__(self, name: str, guard_id: str):
         super().__init__(name, "Guard")
-        self.guard_ID = guard_ID
+        self.guard_id = guard_id
 
     def __str__(self):
-        pass
+        return f"Guard name: {self.name}\nGuard ID: {self.guard_id}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.guard_id})"
 
 class DinningHallStaff(Personnel):
-    def __init__(self, name, staff_ID):
+    def __init__(self, name, staff_id):
         super().__init__(name, "DinningHallStaff")
-        self.staff_ID = staff_ID
+        self.staff_id = staff_id
 
     def __str__(self):
-        pass
+        return f"staff name: {self.name}\nstaff ID: {self.staff_id}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.staff_id})"
 
 class Cook(DinningHallStaff):
-    def __init__(self, name: str, staff_ID: str):
-        super().__init__(name, staff_ID)
+    def __init__(self, name: str, staff_id: str):
+        super().__init__(name, staff_id)
 
     def __str__(self):
-        pass
+        return f"cook name: {self.name}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.staff_id})"
 
 class Server(DinningHallStaff):
-    def __init__(self, name: str, staff_ID: str):
-        super().__init__(name, staff_ID)
+    def __init__(self, name: str, staff_id: str):
+        super().__init__(name, staff_id)
 
     def __str__(self):
-        pass
+        return f"server name: {self.name}"
 
     def __repr__(self):
-        pass
+        return f"{self.__class__.__name__}({self.name},{self.staff_id})"
